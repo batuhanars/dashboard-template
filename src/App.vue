879e8ae69a7023b3ac083b291d90d5b63902aa2b
Batuhan Arslan <batuhan.ars@yahoo.com>
2026-06-08 18:20:32 +0300

    kur(m1): plugin kablolama, anti-flicker script ve env örneği

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-
+  <RouterView />
+  <Sonner rich-colors />
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 import './style.css'
 import App from './App.vue'
+import router from './router'
+import { i18n } from './lib/i18n'
+import { queryClient } from './lib/query-client'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(VueQueryPlugin, { queryClient })
+app.use(i18n)
+
+app.mount('#app')
